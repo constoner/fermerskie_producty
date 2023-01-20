72b8163c87f8fbaa8635e79db169c5b72bfc9e7b
constoner <constantin.kalinin@gmail.com>
2023-01-21 00:08:56 +0300

    развернул проект, подготовил папки, иконки, и т.д.

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,11 @@
-import './App.css';
+import './style.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <h1>Hello, World!</h1>
     </div>
-  );
+  ); 
 }
 
 export default App;
