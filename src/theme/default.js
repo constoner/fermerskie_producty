2f0806e17d0e3d3b0d73e59fe01222e795abc7f1
constoner <constantin.kalinin@gmail.com>
2023-01-27 22:42:51 +0300

    сделал тему

---
+++ src/theme/default.js
@@ -0,0 +1,23 @@
+// стандартная тема
+
+export const defaultTheme = {
+    colorBlackText: "#333333", // основной текст
+    colorAccentText: "#ffffff", // акцентный текст
+    colorBackgroundLite: "#ffffff", // основной фон
+    colorBackgroundDark: "#f6f6f6", // основной темный фон
+    colorBackgroundAccent: "#d8ecfe", // акцентный фон
+    colorCardGoodBackground: "#e1edce", // фон положительной фичи
+    colorCardGoodTag: "#88aa4d", // фон положительного тэга
+    colorCardBadBackground: "#f8ddd7", // фон отрицательной фичи
+    colorCardBadTag: "#f75531", // фон отрицательного тэга
+    colorButton: "#fc9b27", // цвет кнопок
+    colorButtonHover: "#fc7427", // цвет кнопок при наведении
+    pagePadding: "90px", // основный поля страницы
+    indent: "20px", // оступы карточек
+    fontFamily: '"Inter", "Arial", sans-serif', // шрифт
+    fontSizeDefault: "18px", // высота текста
+    fontLineHeghtDefault: "150%",
+    minHeightHeaderFooter: "80px", // высота хэдера и футера
+    pageWidth: "1280px" // ширина страницы
+  };
+  
